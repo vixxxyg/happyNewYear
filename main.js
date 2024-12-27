bedec6baf2049daa3b37e38a1345e82c25b4840d
Viktoriia Hilenko <80678378+vixxxyg@users.noreply.github.com>
2024-12-27 13:32:33 +0100

    3rd

--- main.js
+++ main.js
@@ -1,1 +1,12 @@
-window.onload = console.log(`bu`);
+const button = document.querySelector(".felizNavidad__button");
+const greeting = document.querySelector(".felizNavidad")
+button.addEventListener('click', playMusic);
+
+function playMusic() {
+    const audio = new Audio();
+    audio.preload = "auto";
+    audio.src = "./audio/jingle-bells.mp3";
+    audio.play();
+    greeting.classList.remove("hidden");
+    button.classList.add("hidden");
+}
